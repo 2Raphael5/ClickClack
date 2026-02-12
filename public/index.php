da613f836bce2c1fc4cf8ce2ad621f8f7747cfc7
2Raphael5 <151612958+2Raphael5@users.noreply.github.com>
2026-02-12 14:45:08 +0100

    Modification namespace

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-namespace Raphaelahmn\Jardinier;
+namespace ClickClack\ClickClack;
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
--- route/web.php
+++ route/web.php
@@ -2,8 +2,8 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Raphaelahmn\ClickClack\Controller\AccueilController;
-use Raphaelahmn\ClickClack\Controller\UserController;
+use ClickClack\ClickClack\Controller\AccueilController;
+use ClickClack\ClickClack\Controller\UserController;
 
 
 $app->get('/', [AccueilController::class, 'afficherPagePrincipale']);
