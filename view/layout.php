6099ce853c38ca62898c3b10d57423ce38c83df1
julien-brnct <julien.brnct@eduge.ch>
2026-03-12 15:27:09 +0100

    Design navbar

--- view/layout.php
+++ view/layout.php
@@ -4,35 +4,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Click Clack</title>
     <link rel="stylesheet" href="/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="shortcut icon" href="/img/normale.png" type="image/x-icon">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Click Clack</a>
+            <a class="navbar-brand" href="/">Click Clack</a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li>
-                        <a href="/login"><img src="/img/utilisateur.png" style="width: 4rem;"></a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="/discussion" class="nav-link">Discussions</a>
                     </li>
-                    <li>
-                        <a href="/discussion"  class="navbar-brand">Discussions</a>
-                    </li>
-                    <li>
-                        <a href="/publication/add"  class="navbar-brand">Ajouter une publication</a>
+                    <li class="nav-item">
+                        <a href="/publication/add" class="nav-link">Ajouter une publication</a>
                     </li>
                 </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <?php if (!empty($_SESSION['User'])): ?>
+                            <a href="/profil" class="nav-link d-flex align-items-center">
+                                <img src="/img/<?= htmlspecialchars($_SESSION['User']['photoProfile']) ?>"
+                                    class="rounded-circle object-fit-cover img-fluid" style="width: 50px; height: 50px;"
+                                    alt="">
+                            </a>
+                        <?php else: ?>
+                            <a href="/login" class="nav-link d-flex align-items-center"> <img src="/img/utilisateur.png"
+                                    class="img-fluid" style="max-width: 50px; height: 50px;" alt="">
+                            </a>
+                        <?php endif; ?>
+                    </li>
+                </ul>
+
             </div>
         </div>
     </nav>
+
 
     <?php echo $content ?>
 
